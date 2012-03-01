9986095a63539a4fd1989bcb8e2809ebed61038d
Jon McLaughlin <jon@jpmrc.com>
2012-02-29 23:50:42 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	char	date_str[80];
+
+	if(argc != 2) {
+		printf("Error\n");
+		return -1;
+	}
+
+	time_t date = strtol(argv[1], NULL, 10);
+	struct tm *loc_time = localtime(&date);
+	strftime(date_str, 80, "%Y/%m/%d %H:%M:%S", loc_time);
+	printf("localtime: %s\n", date_str);
+	struct tm *utc_time = gmtime(&date);
+	strftime(date_str, 80, "%Y/%m/%d %H:%M:%S", utc_time);
+	printf(" UTC time: %s\n", date_str);
+
+	return 0;
+}
